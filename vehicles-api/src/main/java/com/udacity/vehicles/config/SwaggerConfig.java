8d3bdc666e8af128dce8a47ed5b333b675cb9cd0
oldrover <oldrover@web.de>
2020-12-16 09:05:12 +0100

    added swagger config

--- vehicles-api/src/main/java/com/udacity/vehicles/config/SwaggerConfig.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/config/SwaggerConfig.java
@@ -1,5 +1,38 @@
 package com.udacity.vehicles.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Vehicles API",
+                "This API can list, create, update nad delete vehicles in an in-memory database",
+                "1.0",
+                "http://www.udacity.com/tos",
+                new Contact("Markus Fleischmann", "www.oldrover.de", "oldrover@web.de" ),
+                "License ofr API", "http://www.udacity.com/license", Collections.emptyList());
+
+    }
 }
